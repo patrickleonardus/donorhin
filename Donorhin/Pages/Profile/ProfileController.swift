23b6e6812365f5caf3875a8b9b28112b06ba3b0d
anandaayupermata <anandayuperm@gmail.com>
2019-11-07 14:27:20 +0700

    add xib file

---
+++ Donorhin/Pages/Profile/ProfileController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileController.swift
+//  Donorhin
+//
+//  Created by Ni Made Ananda Ayu Permata on 07/11/19.
+//  Copyright © 2019 Donorhin. All rights reserved.
+//
+
+import UIKit
+    
+class ProfileController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+}
+
