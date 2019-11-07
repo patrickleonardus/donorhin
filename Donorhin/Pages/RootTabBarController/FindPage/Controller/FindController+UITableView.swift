5b55a3be6fe5fb17c9d349d421a1ee350cf419a7
Patrick Leonardus <patrickleonardus@gmail.com>
2019-11-07 13:47:55 +0700

    add table view, segmented controller, profile image nav bar dan view jika tidak ada data di table view

--- Donorhin/Pages/RootTabBarController/FindPage/Controller/FindController+UITableView.swift
+++ Donorhin/Pages/RootTabBarController/FindPage/Controller/FindController+UITableView.swift
@@ -15,18 +15,62 @@ extension FindController: UITableViewDelegate {
 
 extension FindController: UITableViewDataSource {
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let totalData = bloodRequstData?.count else { fatalError() }
+        
+        var totalData = 0
+        
+        if findBloodSegmentedControl.selectedSegmentIndex == 0 {
+            if let data = bloodRequestCurrent?.count {
+                totalData = data
+            }
+        }
+        
+        else {
+            if let data = bloodRequestHistory?.count {
+                totalData = data
+            }
+        }
+        
         return totalData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell : UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: cellId)
         
-        let data = bloodRequstData![indexPath.row]
-        cell.textLabel?.text = data.name
-        cell.detailTextLabel?.text = data.status
-        return cell
+        let cell  = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? FindBloodCustomCell
+        
+        if findBloodSegmentedControl.selectedSegmentIndex == 0 {
+            
+            guard let data = bloodRequestCurrent?[indexPath.row] else {fatalError()}
+            cell?.title.text = data.name
+            cell?.address.text = data.address
+            cell?.date.text = data.date
+            cell?.status.text = data.status
+            
+            cell?.buttonCallOutlet.setTitle("Call PMI Pendonor", for: .normal)
+            cell?.buttonCallOutlet.isHidden = false
+            cell?.buttonCallOutlet.addTarget(self, action: #selector(callButton), for: .touchUpInside)
+            
+        }
+        
+        else {
+        
+            let data = bloodRequestHistory![indexPath.row]
+            
+            cell?.title.text = data.name
+            cell?.address.text = data.address
+            cell?.date.text = data.date
+            cell?.status.text = data.status
+            
+            cell?.buttonCallOutlet.isHidden = true
+        }
+        
+        cell?.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        
+        return cell!
     }
     
     
