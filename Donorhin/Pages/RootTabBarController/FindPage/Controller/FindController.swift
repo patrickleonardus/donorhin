035526432e16009ad0cac3d1ae1477ae26cec582
Patrick Leonardus <patrickleonardus@gmail.com>
2019-11-11 19:03:21 +0700

    update profile image on the right nav bar

--- Donorhin/Pages/RootTabBarController/FindPage/Controller/FindController.swift
+++ Donorhin/Pages/RootTabBarController/FindPage/Controller/FindController.swift
@@ -40,12 +40,12 @@ class FindController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-      self.setProfileImageNavBar(self.profileImage)
-      setupNavBarToLarge()
+        profileImageNavBar(show: true)
+        setupNavBarToLarge()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-      self.removeProfileImageNavBar(self.profileImage)
+        profileImageNavBar(show: false)
     }
     
     
@@ -76,10 +76,48 @@ class FindController: UIViewController {
         destination.navigationBarTitle =  navBarTitle
     }
     
+    func profileImageNavBar(show: Bool){
+        
+        if show {
+            profileImage = UIImageView(image: UIImage(named: "user_profile_default"))
+            navigationController?.navigationBar.addSubview(profileImage)
+            profileImage.isUserInteractionEnabled = true
+            profileImage.layer.cornerRadius = ProfileImageSize.imageSize/2
+            profileImage.clipsToBounds = true
+            
+            profileImage.translatesAutoresizingMaskIntoConstraints = false
+            profileImage.rightAnchor.constraint(equalTo: (navigationController?.navigationBar.rightAnchor)!, constant: -ProfileImageSize.marginRight).isActive = true
+            profileImage.bottomAnchor.constraint(equalTo: (navigationController?.navigationBar.bottomAnchor)!, constant: -ProfileImageSize.marginBottom).isActive = true
+            profileImage.heightAnchor.constraint(equalToConstant: ProfileImageSize.imageSize).isActive = true
+            profileImage.widthAnchor.constraint(equalToConstant: ProfileImageSize.imageSize).isActive = true
+            
+            let profileTap = UITapGestureRecognizer(target: self, action: #selector(profileButton))
+            profileImage.addGestureRecognizer(profileTap)
+            
+            UIView.animate(withDuration: 1.0) {
+                self.profileImage.alpha = 1.0
+            }
+        }
+        
+        else {
+            UIView.animate(withDuration: 0.1) {
+                self.profileImage.alpha = 0.0
+               
+            }
+        }
+    }
+    
     //MARK: Action
     
     @objc func callButton(){
         callNumber(phoneNumber: "081317019898")
+    }
+    
+    @objc private func profileButton(){
+        let storyboard = UIStoryboard(name: "Profile", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "profileStoryboard") as! ProfileController
+        let navBarOnModal: UINavigationController = UINavigationController(rootViewController: vc)
+        self.present(navBarOnModal, animated: true, completion: nil)
     }
     
     //MARK: Action Outlet
