833278e5ae6a6c611edba42192ec8aaf8c73bb5f
Patrick Leonardus <patrickleonardus@gmail.com>
2019-11-13 18:23:32 +0700

    update

---
+++ Donorhin/Pages/RootTabBarController/AddEventPage/Model/ModelEvent.swift
@@ -0,0 +1,20 @@
+//
+//  ModelEvent.swift
+//  Donorhin
+//
+//  Created by Patrick Leonardus on 13/11/19.
+//  Copyright © 2019 Donorhin. All rights reserved.
+//
+
+import UIKit
+
+struct ModelEvent {
+    
+    let name : String?
+    let placeholder : String?
+    
+}
+
+struct EventData {
+    
+}
