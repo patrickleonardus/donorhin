27abaa0567bfd1bb9d6bc5800aa636bc480bb3b0
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-11-13 18:20:06 +0700

    Create skeleton for donate page (will use page view controller)

---
+++ Donorhin/Pages/RootTabBarController/DonatePage/RequestStepsPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RequestStepsPageViewController.swift
+//  Donorhin
+//
+//  Created by Vebby Clarissa on 13/11/19.
+//  Copyright © 2019 Donorhin. All rights reserved.
+//
+
+import UIKit
+
+class RequestStepsPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
