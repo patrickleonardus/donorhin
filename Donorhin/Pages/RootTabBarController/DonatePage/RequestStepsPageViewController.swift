2b408a72d498a1ecc97562c759533359fbf92c57
idris11 <idrisnovandi@gmail.com>
2019-11-13 20:27:32 +0700

    add page view controller

--- Donorhin/Pages/RootTabBarController/DonatePage/RequestStepsPageViewController.swift
+++ Donorhin/Pages/RootTabBarController/DonatePage/RequestStepsPageViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class RequestStepsPageViewController: UIPageViewController {
-   
-   lazy var vcList: [UIViewController] = []
-      //let sb = UIStoryboard
-      //return
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+  var id: Int!
+  lazy var vcList: [UIViewController] = {
+    let sb = UIStoryboard(name: "RequestStepsPageViewController", bundle: nil)
+    switch self.id {
+      case 1:
+        return [sb.instantiateViewController(withIdentifier: "1")]
+      case 2:
+      return [sb.instantiateViewController(withIdentifier: "2")]
+      case 3:
+        return [sb.instantiateViewController(withIdentifier: "3")]
+      case 4:
+        return [sb.instantiateViewController(withIdentifier: "4")]
+      case 5:
+      return [sb.instantiateViewController(withIdentifier: "5")]
+    default:
+      return [sb.instantiateViewController(withIdentifier: "1")]
     }
-    
+  }()
+      
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let firstViewController = self.vcList.first {
+      self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+    }
+  }
 }
