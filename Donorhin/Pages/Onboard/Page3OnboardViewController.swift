065e6887e9b7c6c35e7a6ba38b03ff14ca5a9e67
anandaayupermata <anandayuperm@gmail.com>
2019-11-14 18:49:57 +0700

    font size constraint

---
+++ Donorhin/Pages/Onboard/Page3OnboardViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Page1ViewController.swift
+//  Donorhin
+//
+//  Created by Ni Made Ananda Ayu Permata on 14/11/19.
+//  Copyright © 2019 Donorhin. All rights reserved.
+//
+
+import UIKit
+
+
+class Page3ViewController: UIViewController {
+
+    @IBOutlet weak var privasiLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if UDDevice.widthScreen < 400 {
+            privasiLabel.font = privasiLabel.font.withSize(17)
+        }
+    }
+}
---
+++ Donorhin/Pages/Onboard/Page4OnboardViewController.swift
@@ -0,0 +1,28 @@
+//
+//  Page1ViewController.swift
+//  Donorhin
+//
+//  Created by Ni Made Ananda Ayu Permata on 14/11/19.
+//  Copyright © 2019 Donorhin. All rights reserved.
+//
+
+import UIKit
+
+
+class Page4ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if UDDevice.widthScreen < 400 {
+            label1.font = label1.font.withSize(17)
+            label2.font = label2.font.withSize(17)
+            label3.font = label3.font.withSize(17)
+
+        }
+    }
+}
