894e8876a7108c7cbbcb92fdfe3d897e98598bf1
Patrick Leonardus <patrickleonardus@gmail.com>
2019-11-15 16:27:39 +0700

    update

--- Donorhin/Pages/Authentication/RegisterPage/Controller/RegisterController.swift
+++ Donorhin/Pages/Authentication/RegisterPage/Controller/RegisterController.swift
@@ -45,30 +45,30 @@ class RegisterController : UIViewController{
     
 }
 
-class CheckBox: UIButton {
-    // Images
-    let checkedImage = UIImage(named: "checkbox_isChecked")! as UIImage
-    let uncheckedImage = UIImage(named: "checkbox_isNotChecked")! as UIImage
-
-    // Bool property
-    var isChecked: Bool = false {
-        didSet{
-            if isChecked == true {
-                self.setImage(checkedImage, for: UIControl.State.normal)
-            } else {
-                self.setImage(uncheckedImage, for: UIControl.State.normal)
-            }
-        }
-    }
-
-    override func awakeFromNib() {
-        self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
-        self.isChecked = false
-    }
-
-    @objc func buttonClicked(sender: UIButton) {
-        if sender == self {
-            isChecked = !isChecked
-        }
-    }
-}
+//class CheckBox: UIButton {
+//    // Images
+//    let checkedImage = UIImage(named: "checkbox_isChecked")! as UIImage
+//    let uncheckedImage = UIImage(named: "checkbox_isNotChecked")! as UIImage
+//
+//    // Bool property
+//    var isChecked: Bool = false {
+//        didSet{
+//            if isChecked == true {
+//                self.setImage(checkedImage, for: UIControl.State.normal)
+//            } else {
+//                self.setImage(uncheckedImage, for: UIControl.State.normal)
+//            }
+//        }
+//    }
+//
+//    override func awakeFromNib() {
+//        self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
+//        self.isChecked = false
+//    }
+//
+//    @objc func buttonClicked(sender: UIButton) {
+//        if sender == self {
+//            isChecked = !isChecked
+//        }
+//    }
+//}
