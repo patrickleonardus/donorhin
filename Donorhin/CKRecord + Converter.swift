08031c03f55783ec41740fcbc37d43d81413e646
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-11-19 18:30:32 +0700

    Refractored code

--- Donorhin/CKRecord + Converter.swift
+++ Donorhin/CKRecord + Converter.swift
@@ -22,27 +22,16 @@ extension CKAsset {
 
 extension CKRecord {
    func convertTrackerToTrackerModel() -> TrackerModel? {
-      guard let donordate = self.value(forKey: "donor_date") as? Date ?? nil
+      guard
+         let donordate = self.value(forKey: "donor_date") as? Date ?? nil,
+         let idpendonor = self.value(forKey: "id_pendonor") as? Reference ?? nil,
+         let idrequest = self.value(forKey: "id_request") as? Reference ?? nil,
+         let idUTDPendonor = self.value(forKey: "id_UTD_pendonor") as? Reference ?? nil,
+         let currentStep = self.value(forKey: "current_step") as? Int ?? nil
          else {
 //            fatalError("failed casting data with record ID \(self.recordID)")
-            return nil }
-      guard let idpendonor = self.value(forKey: "id_pendonor") as? Reference ?? nil
-         else {
-//            fatalError("failed casting data with record ID \(self.recordID)")
-            return nil }
-      guard let idrequest = self.value(forKey: "id_request") as? Reference ?? nil
-         else {
-//            fatalError("failed casting data with record ID \(self.recordID)")
-            return nil }
-      guard let idUTDPendonor = self.value(forKey: "id_UTD_pendonor") as? Reference ?? nil
-         else {
-//            fatalError("failed casting data with record ID \(self.recordID)")
-            return nil }
-      guard let currentStep = self.value(forKey: "current_step") as? Int ?? nil
-         else {
-//            fatalError("failed casting data with record ID \(self.recordID)")
-            return nil }
-      
+            return nil
+      }
       let trackerModel = TrackerModel(idTracker: self.recordID,
                                       donorDate: donordate,
                                       idPendonor: idpendonor,
@@ -53,19 +42,15 @@ extension CKRecord {
    }
    
    func convertRequestToRequestModel() -> RequestModel? {
-      guard let patientName = self.value(forKey: "patient_name") as? String ?? nil
-         else { return nil }
-      guard let bloodTypePatient =
-         (self.value(forKey: "patient_blood_type") as? String ?? nil)?.convertToBloodType()
-         else { return nil }
-      guard let amount = self.value(forKey: "amount") as? Int ?? nil
-         else { return nil }
-      guard let dateNeed = self.value(forKey: "date_need") as? Date ?? nil
-         else { return nil }
-      guard let isEmergency = self.value(forKey: "isEmergency") as? Int ?? nil
-         else { return nil }
-      guard let idUTDPatient = self.value(forKey: "UTD_patient") as? Reference ?? nil
-         else { return nil }
+      guard let patientName = self.value(forKey: "patient_name") as? String ?? nil,
+         let bloodTypePatient = (self.value(forKey: "patient_blood_type") as? String ?? nil)?.convertToBloodType(),
+         let amount = self.value(forKey: "amount") as? Int ?? nil,
+         let dateNeed = self.value(forKey: "date_need") as? Date ?? nil,
+         let isEmergency = self.value(forKey: "isEmergency") as? Int ?? nil,
+         let idUTDPatient = self.value(forKey: "UTD_patient") as? Reference ?? nil
+         else {
+            return nil
+      }
       
       let requestModel = RequestModel(idRequest: self.recordID,
                                       patientName: patientName,
@@ -80,14 +65,19 @@ extension CKRecord {
    
    func convertEventToEventGlobalModel ()  -> EventGlobalModel?{
       
-      guard let title = self.value(forKey: "title") as? String ?? nil else { return nil }
-      guard let description = self.value(forKey: "description") as? String ?? nil else { return nil }
-      guard let address = self.value(forKey: "address") as? String ?? nil else { return nil }
-      guard let startTime = self.value(forKey: "start_time") as? Date ?? nil else { return nil }
-      guard let endTime = self.value(forKey: "end_time") as? Date ?? nil else { return nil }
-      guard let cpPhone = self.value(forKey: "contact_phone") as? String ?? nil else { return nil }
-      guard let cpName = self.value(forKey: "contact_name") as? String ?? nil else { return nil }
-      guard let idUser = self.value(forKey: "reference_account") as? Reference ?? nil else { return nil }
+      guard
+         let title = self.value(forKey: "title") as? String ?? nil,
+         let description = self.value(forKey: "description") as? String ?? nil,
+         let address = self.value(forKey: "address") as? String ?? nil,
+         let startTime = self.value(forKey: "start_time") as? Date ?? nil,
+         let endTime = self.value(forKey: "end_time") as? Date ?? nil,
+         let cpPhone = self.value(forKey: "contact_phone") as? String ?? nil,
+         let cpName = self.value(forKey: "contact_name") as? String ?? nil,
+         let idUser = self.value(forKey: "reference_account") as? Reference ?? nil
+         else {
+            return nil
+      }
+      
       let image : CKAsset? = self.object(forKey: "image") as? CKAsset
       
       let eventModel = EventGlobalModel(idEvent: self.recordID,
@@ -105,19 +95,19 @@ extension CKRecord {
    }
    
    func convertAccountToUserModel() -> UserModel? {
-      guard let name = self.value(forKey: "name") as? String ?? nil else { return nil }
-      guard let bloodType =
-         (self.value(forKey: "blood_type") as? String ?? nil)?.convertToBloodType()
-         else { return nil }
-      guard let email = self.value(forKey: "email") as? String ?? nil else { return nil }
-      guard let password = self.value(forKey: "password") as? String ?? nil else { return nil }
-      guard let birthdate = self.value(forKey: "birth_date") as? Date ?? nil else { return nil }
-      guard let lastDonor = self.value(forKey: "last_donor") as? Date ?? nil else { return nil }
-
-      guard let isVerif = self.value(forKey: "isVerified") as? Int ?? nil else { return nil }
-      
-      guard let gdr = self.value(forKey: "gender") as? Int ?? nil else { return nil }
-      
+      guard let name = self.value(forKey: "name") as? String ?? nil,
+         let bloodType = (self.value(forKey: "blood_type") as? String ?? nil)?.convertToBloodType(),
+         let email = self.value(forKey: "email") as? String ?? nil,
+         let password = self.value(forKey: "password") as? String ?? nil,
+         let birthdate = self.value(forKey: "birth_date") as? Date ?? nil,
+         let lastDonor = self.value(forKey: "last_donor") as? Date ?? nil,
+         let isVerif = self.value(forKey: "isVerified") as? Int ?? nil,
+         let gdr = self.value(forKey: "gender") as? Int ?? nil,
+         let statusDonor = self.value(forKey: "donor_status") as? Int ?? nil
+         else {
+            return nil
+      }
+      let location = self.value(forKey: "location") as? CLLocation
       let gender = { () -> Gender in
          if gdr == 0 {
             return Gender.female
@@ -125,11 +115,6 @@ extension CKRecord {
             return Gender.male
          }
       }
-      
-      let location = self.value(forKey: "location") as? CLLocation
-      
-      guard let statusDonor = self.value(forKey: "donor_status") as? Int ?? nil else { return nil }
-      
       let userModel = UserModel(idUser: self.recordID,
                                 name: name,
                                 location: location,
@@ -145,11 +130,15 @@ extension CKRecord {
    }
    
    func convertUTDToUTDModel () -> UTDModel? {
-      guard let name = self.value(forKey: "name") as? String ?? nil else { return nil }
-      guard let address = self.value(forKey: "address") as? String ?? nil else { return nil }
-      guard let location = self.object(forKey: "location") as? CLLocation ?? nil else { return nil }
-      guard let phoneNumbers = self.object(forKey: "telephone") as? NSArray ?? nil else { return nil }
-      guard let province = self.value(forKey: "province") as? String ?? nil else { return nil }
+      guard
+         let name = self.value(forKey: "name") as? String ?? nil,
+         let address = self.value(forKey: "address") as? String ?? nil,
+         let location = self.object(forKey: "location") as? CLLocation ?? nil,
+         let phoneNumbers = self.object(forKey: "telephone") as? NSArray ?? nil,
+         let province = self.value(forKey: "province") as? String ?? nil
+         else {
+            return nil
+      }
 
       var phones : [String]?
       if phoneNumbers.count == 0 {
