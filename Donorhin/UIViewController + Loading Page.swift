c2256495169c97ae85c24488460b5819a2dac26a
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-11-20 15:12:49 +0700

    fixing bug

--- Donorhin/UIViewController + Loading Page.swift
+++ Donorhin/UIViewController + Loading Page.swift
@@ -13,7 +13,7 @@ var vSpinner : UIView? //global variable
 extension UIViewController {
    func showSpinner(onView : UIView) {
       let spinnerView = UIView.init(frame: onView.bounds)
-      spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+      spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.40)
       let ai = UIActivityIndicatorView.init(style: .whiteLarge)
       ai.startAnimating()
       ai.center = spinnerView.center
