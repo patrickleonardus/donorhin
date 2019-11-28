b4ad95f8ad511bbb591adc73d98f6e957781bb55
anandaayupermata <anandayuperm@gmail.com>
2019-11-28 18:17:03 +0700

    add comment and change content

--- Donorhin/Pages/Onboard/Page2OnboardViewController.swift
+++ Donorhin/Pages/Onboard/Page2OnboardViewController.swift
@@ -16,6 +16,8 @@ class Page2ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // MARK : - CHANGE FONT SIZE
         if UDDevice.widthScreen < 400 {
             lacakLabel.font = lacakLabel.font.withSize(17)
         }
--- Donorhin/Pages/Onboard/Page3OnboardViewController.swift
+++ Donorhin/Pages/Onboard/Page3OnboardViewController.swift
@@ -16,6 +16,8 @@ class Page3ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // MARK : - CHANGE FONT SIZE
         if UDDevice.widthScreen < 400 {
             privasiLabel.font = privasiLabel.font.withSize(17)
         }
