1f580554452a88010883e0389209fd656336b6bb
Patrick Leonardus <patrickleonardus@gmail.com>
2019-12-12 21:00:09 +0700

    gajadi dihapus guys, error trnyata

---
+++ Donorhin/String + Time Removal.swift
@@ -0,0 +1,21 @@
+//
+//  Date + Time Removal.swift
+//  Donorhin
+//
+//  Created by Vebby Clarissa on 25/11/19.
+//  Copyright © 2019 Donorhin. All rights reserved.
+//
+import Foundation
+
+extension String {
+  var formattedDate: String? {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ssZZZZ"
+    if let date = dateFormatter.date(from: self) {
+      print(date)
+      dateFormatter.dateFormat = "dd MMMM yyyy"
+      return dateFormatter.string(from: date)
+    }
+    return nil
+  }
+}
