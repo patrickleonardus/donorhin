59cc1c0debbd76912c36dedf155a20b53a91f6a4
Patrick Leonardus <patrickleonardus@gmail.com>
2019-12-15 22:02:44 +0700

    apdet lagi boiiii

--- Donorhin/UIViewController + Loading Page.swift
+++ Donorhin/UIViewController + Loading Page.swift
@@ -41,16 +41,42 @@ extension UIViewController {
       onView.bringSubviewToFront(ai)
     }
     ai.startAnimating()
-//    vSpinner = loadingView
-//    bgSpinner = spinnerView
+  }
+  
+  func showSpinner(onView : UIView, x : Int, y : Int){
+    let spinnerView = UIView.init(frame: onView.bounds)
+    spinnerView.backgroundColor = UIColor.init(red: 0.8, green: 0.8, blue: 0.8, alpha: 0.1)
+
+    let loadingView: UIView = UIView()
+    loadingView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+    loadingView.center = CGPoint(x: x, y: y)
+    loadingView.backgroundColor = UIColor.init(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.6)
+    loadingView.clipsToBounds = true
+    loadingView.layer.cornerRadius = 10
+
+
+    let ai : UIActivityIndicatorView = UIActivityIndicatorView()
+    ai.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+    ai.style = .whiteLarge
+    ai.center = CGPoint(x: loadingView.frame.size.width/2, y: loadingView.frame.size.height/2)
+
+
+    DispatchQueue.main.async {
+      loadingView.tag = 99
+      spinnerView.tag = 100
+      loadingView.addSubview(ai)
+      spinnerView.addSubview(loadingView)
+      onView.addSubview(spinnerView)
+      onView.bringSubviewToFront(spinnerView)
+      onView.bringSubviewToFront(ai)
+    }
+    ai.startAnimating()
   }
   
   func removeSpinner() {
     DispatchQueue.main.async {
       self.view.viewWithTag(99)?.removeFromSuperview()
       self.view.viewWithTag(100)?.removeFromSuperview()
-//      vSpinner?.removeFromSuperview()
-//      bgSpinner?.removeFromSuperview()
       vSpinner = nil
       bgSpinner = nil
     }
