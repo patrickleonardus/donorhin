d8c5c3931c22b11204af5f981e28966013c668ab
Idris <idrisnovandi@gmail.com>
2019-12-16 15:10:36 +0700

    push notification 4th step for recipient

--- Donorhin/Pages/RootTabBarController/DonatePage/FourthStepRequestViewController.swift
+++ Donorhin/Pages/RootTabBarController/DonatePage/FourthStepRequestViewController.swift
@@ -7,10 +7,35 @@
 //
 
 import UIKit
+import CloudKit
 
 class FourthStepRequestViewController: DonateStepViewController {
+	var requestNotification: String?
+	var tokenNotification: String?
+	
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+	
+	override func recieveRequest(_ tracker: TrackerModel?) {
+		self.getDetailRequest(tracker?.idRequest)
+	}
+	
+	//MARK: - initiate data for send notification
+	private func getDetailRequest(_ idRequest: CKRecord.Reference?) {
+    guard let idRequest = idRequest else {return}
+		self.requestNotification = idRequest.recordID.recordName
+		Helper.getDataByID(idRequest.recordID) { (records) in
+			if let record = records {
+				let userId = record.value(forKey: "userId") as! CKRecord.Reference
+				Helper.getDataByID(userId.recordID) {[weak self] (recordAccount) in
+					if let recordAccount = recordAccount {
+						let deviceToken = recordAccount.value(forKey: "device_token") as! String
+						self?.tokenNotification = deviceToken
+					}
+				}
+			}
+		}
   }
   
   @IBAction func buttonAcceptTapped(_ sender: UIButton) {
@@ -30,6 +55,10 @@ class FourthStepRequestViewController: DonateStepViewController {
     let accept = UIAlertAction( title: "Ya", style: .default) { (action) in
       //TODO:  Write code to accept here
       self.pageViewDelegate?.changeShowedView(toStep: 5,tracker: nil)
+			if let token = self.tokenNotification,
+				let idRequest = self.requestNotification {
+				Service.sendNotification("Pendonor sudah melakukan donor darah", [token], idRequest, 0)
+			}
     }
     
     let cancel = UIAlertAction(title: "Tidak", style: .cancel, handler: nil)
